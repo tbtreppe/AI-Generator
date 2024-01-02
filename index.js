1d53e94c8bcb27b5a5b5a2c4caee8b5bee1a7da2
Tracey <ttrepper@yahoo.com>
2024-01-01 20:54:47 -0500

    add AI api and instructions

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-function generateAnswer(event) {
-  event.preventDefault();
-
-  new Typewriter("#answer", {
-    strings: "hello",
+function displayAnswer(response) {
+  new Typewriter("#content", {
+    strings: response.data.answer,
     autoStart: true,
     cursor: null,
     delay: 20,
   });
+}
+
+function generateAnswer(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "babba12d05727fe813b5e3b80fo05ta4";
+  let prompt = `Generate a short recipe based on ${instructionsInput.value}`;
+  let context =
+    "You are a chef and like to create short recipes in basic HTML and seperate each line with a </br>. Please follow the user instructions";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let contentElement = document.querySelector("#content");
+  contentElement.innerHTML = `<div class = "generating">Generating a recipe about ${instructionsInput.value}`;
+
+  axios.get(apiUrl).then(displayAnswer);
 }
 
 let formElement = document.querySelector("#form");
